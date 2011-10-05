d29c29aae8bae6e55d8e6adc12599792b3df28e4
qtonneau@gmail.com <qtonneau@gmail.com@7b36dba4-9ef3-3368-da70-6ac0c8623dca>
2011-10-05 13:22:34 +0000

    avancée des fonctions et structure

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ struct cellule_arete {
 }
 struct cellule_sommet {
 	int numero;
-	cellule_arete liste;
+	cellule_arete liste_arete;
 	cellule_sommet suivante;
 }
 struct liste_sommet {
@@ -15,4 +15,22 @@ struct liste_sommet {
 	cellule_sommet fin;
 	int taille;
 }
+liste_sommet liste;
 
+// Ajoute un sommet à la fin de la liste donnée, avec le numéro donné
+void ajouter_sommet(int numero, liste_sommet liste)
+{
+	//@TODO a vérifier
+	cellule_sommet nouveau;
+	nouveau.numero=numero;
+	liste.fin.suivante=nouveau;
+	liste.fin=nouveau;
+}
+
+// Ajoute une arete au début de la liste donnée
+void ajouter_arete(char* etiquette, cellule_arete liste_arete)
+{
+	cellule_arete nouveau;
+	nouveau.etiquette = etiquette;
+	//@TODO A finir
+}
